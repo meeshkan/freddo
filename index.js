8a0fde4f33f31c6eae0de0e83c34b8cb41fb2875
k4m4 <nikolaskam@gmail.com>
2019-10-16 16:07:15 -0400

    Create request function prototype that populates Test function data

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const got = require('got')
+
 const freddo = url => new Test(url)
 
 function Test(url) {
@@ -8,4 +10,28 @@ function Test(url) {
 		statusCode: null,
 		error: null
 	}
+	this.promise = Promise.resolve(null)
+	this.request()
+}
+
+Test.prototype.extend = function(promise, that) {
+    for (const key in that) {
+        promise[key] = that[key]
+    }
+}
+
+Test.prototype.next = function(what) {
+    this.promise = this.promise.then(what)
+    this.extend(this.promise, this)
+    return this.promise
+}
+
+Test.prototype.request = function() {
+    return this.next(async () => {
+        let response = await got(this.dataObj.url)
+        this.dataObj.headers = response.headers
+        this.dataObj.body = response.body
+        this.dataObj.statusCode = response.statusCode
+        return true
+    })
 }
