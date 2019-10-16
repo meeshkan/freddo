e541e2ec72a4a27d7df2c8465b895aab5d11dfaa
k4m4 <nikolaskam@gmail.com>
2019-10-16 16:49:57 -0400

    Create body test

--- test/test.js
+++ test/test.js
@@ -3,3 +3,32 @@ const { clearModuleCache, stubModule } = require('./runkit')
 
 const clearFreddoCache = () => clearModuleCache('../index')
 const stubGot = (returnsVal) => stubModule('got', returnsVal)
+
+let freddo, expr, toExist
+
+t('body', async t => {
+    clearFreddoCache()
+    stubGot({
+        headers: {},
+        statusCode: '',
+        body: { foo: 'bar' }
+    })
+    ;({ freddo } = require('../index'))
+	t.is(await freddo().body({'foo': 'bar'}), true)
+})
+
+t('body with no match', async t => {
+    clearFreddoCache()
+    stubGot({
+        headers: {},
+        statusCode: '',
+        body: { foo: 'bar' }
+    })
+    ;({ freddo } = require('../index'))
+	const error = await t.throwsAsync(async () => { 
+		await freddo()
+			.expect('body', {'foo': 'unicorn'})
+            .ensure()
+    })
+	t.is(error.message, 'Expected key "body" to be {"foo":"unicorn"}, but got {"foo":"bar"}')
+})
