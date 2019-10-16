ed1c1d2dd7b6112b7032a85864ecb8f9b9a4203f
k4m4 <nikolaskam@gmail.com>
2019-10-16 16:49:32 -0400

    Create stubGot function

--- test/test.js
+++ test/test.js
@@ -1,2 +1,5 @@
 import * as t from 'ava'
 const { clearModuleCache, stubModule } = require('./runkit')
+
+const clearFreddoCache = () => clearModuleCache('../index')
+const stubGot = (returnsVal) => stubModule('got', returnsVal)
